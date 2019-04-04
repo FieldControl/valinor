e45e85ea6277c318ac9a3bc6f39b49f7a1a647bc
Rodolfo Menardi <rodolfomenardi@gmail.com>
2019-04-04 01:58:41 -0300

    Foram adicionados o Helmet e o Compression para fornecer um pouco de segurança a aplicação e também compactação para economia de banda.

--- Challenge-Backend-Developer-1/index.js
+++ Challenge-Backend-Developer-1/index.js
@@ -3,22 +3,25 @@ require('dotenv').config({
 });
 
 let express = require('express');
+let helmet = require("helmet");
+let compression = require("compression");
 let bodyParser = require('body-parser');
 let mongoose = require('mongoose');
 let app = express();
 let routes = require("./api/routes")
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.json());
-
 mongoose.connect(`mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_IP}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {useNewUrlParser: true});
 var db = mongoose.connection;
 
 var port = process.env.APP_PORT || 8080;
-app.use('/', routes);
+app.use(helmet());
+app.use(compression());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 app.use(express.static("public"));
+app.use('/', routes);
 app.listen(port, function () {
     console.log("Running challenge on port " + port);
 });
