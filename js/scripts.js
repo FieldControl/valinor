f313c5e8bf40223a32908d3761da6a0b130f1ad1
Rebecca <rebecca.ignacio@etec.sp.gov.br>
2020-03-12 20:12:35 -0300

    Versão Otimizada - Desafio Field Rebecca
    JS e CSS minified.
    Index código HTMl indentado.

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,10 @@
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
-
-/* QUOTES */
-
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
-  
-      /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-    function openNav() {
-      document.getElementById("mySidebar").style.width = "250px";
-      document.getElementById("main").style.marginLeft = "250px";
-    }
-
-    /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-    function closeNav() {
-      document.getElementById("mySidebar").style.width = "0";
-      document.getElementById("main").style.marginLeft = "0";
-    }
-  
+var slideIndex=1;showSlides(slideIndex);function plusSlides(n){showSlides(slideIndex+=n)}
+function currentSlide(n){showSlides(slideIndex=n)}
+function showSlides(n){var i;var slides=document.getElementsByClassName("mySlides");var dots=document.getElementsByClassName("dot");if(n>slides.length){slideIndex=1}
+if(n<1){slideIndex=slides.length}
+for(i=0;i<slides.length;i++){slides[i].style.display="none"}
+for(i=0;i<dots.length;i++){dots[i].className=dots[i].className.replace(" active","")}
+slides[slideIndex-1].style.display="block";dots[slideIndex-1].className+=" active"}
+function topFunction(){document.body.scrollTop=0;document.documentElement.scrollTop=0}
+function openNav(){document.getElementById("mySidebar").style.width="250px";document.getElementById("main").style.marginLeft="250px"}
+function closeNav(){document.getElementById("mySidebar").style.width="0";document.getElementById("main").style.marginLeft="0"}
