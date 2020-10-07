07f64be4abf3e4419cb15ec7315ddc9244c124e2
Walefe <walefedev@gmail.com>
2020-10-07 13:19:06 -0300

    feat: config localstorage for datas.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,22 @@
-import React, { FormEvent, useCallback, useRef, useState } from 'react';
+import React, {
+  FormEvent,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import { FiLoader } from 'react-icons/fi';
 
 import Repository from '../../components/Repository';
 import api from '../../services/api';
+import formatNumber from '../../utils/format';
 
 import logo from '../../assets/logo.png';
 
 import { Container } from './styles';
 
 interface IDataProps {
+  formatedCount: string;
   total_count: number;
   items: IRepositorieDataProps[];
 }
@@ -23,15 +31,32 @@ interface IRepositorieDataProps {
   description: string;
   stargazers_count: number;
   watchers_count: number;
+  stargazers_format_count: string;
+  watchers_format_count: string;
 }
 
 const Dashboard: React.FC = () => {
   const inputRepositoryRef = useRef<HTMLInputElement>(null);
 
   const [loading, setLoading] = useState(false);
-  const [repositories, setRepositories] = useState<IDataProps>(
-    {} as IDataProps,
-  );
+  const [repositories, setRepositories] = useState<IDataProps>(() => {
+    const storageRepositories = localStorage.getItem(
+      '@GithubExplore:repositories',
+    );
+
+    if (storageRepositories) {
+      return JSON.parse(storageRepositories);
+    }
+
+    return {} as IDataProps;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplore:repositories',
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
 
   const handleSearchRepository = useCallback(async (event: FormEvent): Promise<
     void
@@ -40,16 +65,30 @@ const Dashboard: React.FC = () => {
     setLoading(true);
     const findRepository = inputRepositoryRef.current?.value;
 
-    const response = await api.get('search/repositories', {
+    const response = await api.get<IDataProps>('search/repositories', {
       params: { q: findRepository, per_page: 5 },
     });
-    setRepositories(response.data);
+
+    const { items, total_count } = response.data;
+
+    const formatResponse = {
+      items: items.map(item => ({
+        ...item,
+        stargazers_format_count: formatNumber(item.stargazers_count),
+        watchers_format_count: formatNumber(item.watchers_count),
+      })),
+      formatedCount: formatNumber(total_count),
+      total_count,
+    };
+
+    setRepositories(formatResponse);
     setLoading(false);
   }, []);
+
   return (
     <Container loading={loading}>
       <header>
-        <img src={logo} alt="Walefe" />
+        <img src={logo} alt="GitHub" />
         <h1>Github Explorer</h1>
       </header>
       <form onSubmit={handleSearchRepository}>
@@ -63,7 +102,10 @@ const Dashboard: React.FC = () => {
       {loading ? (
         <FiLoader size={36} color="#3a3a3a" />
       ) : (
-        <Repository data={repositories} />
+        <>
+          <h3>{repositories.formatedCount}</h3>
+          <Repository data={repositories} />
+        </>
       )}
     </Container>
   );
