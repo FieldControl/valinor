fc277cedd07833a1a3fba05f2dfbea510b23bc0f
Walefe <walefedev@gmail.com>
2020-10-08 10:44:40 -0300

    feat: create page issues.

---
+++ src/pages/Issues/index.tsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react';
+import { useRouteMatch } from 'react-router-dom';
+
+import api from '../../services/api';
+
+import { Wrapper, Container, ContainerCard, HeaderCard } from './styles';
+
+interface IRepositoryParams {
+  repository: string;
+}
+
+interface Issue {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+  };
+  labels: ILabels[];
+  state: string;
+}
+
+interface ILabels {
+  name: string;
+  color: string;
+}
+
+const Issues: React.FC = () => {
+  const { params } = useRouteMatch<IRepositoryParams>();
+
+  const [issues, setIssues] = useState<Issue[]>([]);
+
+  useEffect(() => {
+    api.get(`repos/${params.repository}/issues`).then(response => {
+      setIssues(response.data);
+    });
+  }, [params.repository]);
+
+  return (
+    <Wrapper>
+      <Container>
+        {issues.map(issue => (
+          <ContainerCard href={issue.html_url} target="_blank" key={issue.id}>
+            <HeaderCard state={issue.state}>
+              <h2>{issue.user.login}</h2>
+              <span>{issue.state}</span>
+            </HeaderCard>
+            <p>{issue.title}</p>
+          </ContainerCard>
+        ))}
+      </Container>
+    </Wrapper>
+  );
+};
+
+export default Issues;
---
+++ src/pages/Issues/styles.ts
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+interface IState {
+  state: string;
+}
+
+export const Wrapper = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-gap: 20px;
+  max-width: 1280px;
+  padding: 24px 12px;
+  margin: 0 auto;
+`;
+
+export const ContainerCard = styled.a`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  padding: 24px;
+  margin: 0 auto;
+  border: 1px solid #3a3a3a;
+  border-radius: 5px;
+  background-color: #ffff;
+  text-decoration: none;
+  color: #3a3a3a;
+
+  p {
+    margin: 18px 0;
+    color: #3a3a3a;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+`;
+
+export const HeaderCard = styled.header<IState>`
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+
+  span {
+    display: block;
+    background-color: ${props => (props.state === 'open' ? '#3e0' : '#ff0033')};
+    color: #ffff;
+    padding: 6px;
+    border-radius: 8px;
+  }
+`;
