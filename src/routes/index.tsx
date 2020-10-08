6bec4a9db3a84fe37f86bf509744c1f2e5250b09
Walefe <walefedev@gmail.com>
2020-10-08 10:45:37 -0300

    feat: add route for pade issue.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Dashboard from '../pages/Dashboard';
+import Issues from '../pages/Issues';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Dashboard} />
+    <Route path="/issues/:repository+" component={Issues} />
   </Switch>
 );
 
