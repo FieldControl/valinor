e0b5237ee78eb63c0a2c50b1da25d7c3af431879
Elian Becali <elianbecaliaguiar@gmail.com>
2021-05-10 20:10:07 -0400

    refactor: move browser router component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
-import { BrowserRouter } from 'react-router-dom';
 import { Routes } from './routes';
 import GlobalStyle from './styles/globalStyle';
 
 export function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
+      <Routes />
       <GlobalStyle />
     </>
   );
