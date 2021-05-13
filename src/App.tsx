d37bd92eb8b5c53195f3f2c835b446b0582e946a
BismarckOliveira <bismarckoliver@hotmail.com>
2021-05-12 22:15:03 -0300

    Add_Layout_GitExplore

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Dashboard } from "./components/Dashboard"
+import { Header } from "./components/Header"
+import {GlobalStyle} from "./styles/global"
+
 export const App: React.FC = () => {
   return (
-    <h1>Hello Word</h1>
+  <>  
+    <Header />
+    <Dashboard />
+
+    <GlobalStyle/>
+  </>
   )
 }
---
+++ src/styles/global.ts
@@ -0,0 +1,61 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+
+:root{
+  --background: #0d1117;
+  --text-primary:#f0f6fc;
+  --text-secondary: #8b9496;
+  --gray-700: #161b22; 
+  --gray-500: #21262d;
+  --gray-300: #30363d;
+  --gray-250: #6a737d;
+  --gray-200: #c9d1d9;
+  --blue-300: #58a6ff;
+  --blue-100: rgba(56,139,253,0.1);
+   
+} 
+
+*{
+  margin: 0;
+  padding: 0;
+  outline: 0;
+  box-sizing: border-box;
+}
+
+html{
+  @media (max-width: 1080px){
+    font-size: 93.75%;
+  }
+
+  @media (max-width: 720px){
+    font-size: 87.05%;
+  }
+}
+
+body{
+  -webkit-font-smoothing: antialiased;
+  background: var(--background);
+
+}
+
+body, textarea, input, button{
+  font-family: 'Roboto', sans-serif;
+  color: var()
+}
+
+
+h1, h2, h3, h4,h5, h6,strong{
+  font-weight: 700;
+}
+
+ul{
+  list-style: none;
+}
+
+
+button{
+  cursor: pointer;
+}
+
+`;
