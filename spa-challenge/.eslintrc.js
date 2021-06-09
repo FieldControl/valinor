6010755f3f1d99d5480a2d0ed0d2c1e52afc83d6
William Augusto <william.santos315@outlook.com>
2021-06-09 18:55:13 -0300

    airbnb styleguide (js and ts)

--- spa-challenge/.eslintrc.js
+++ spa-challenge/.eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 2020,
+    parser: '@typescript-eslint/parser',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
