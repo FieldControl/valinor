a22da0332a25ed3ed464fdb1a7e4569dcf86c93c
William Augusto <william.santos315@outlook.com>
2021-06-09 21:51:39 -0300

    api supertest

--- spa-challenge/tests/unit/api.spec.ts
+++ spa-challenge/tests/unit/api.spec.ts
@@ -1,7 +1,16 @@
+import supertest from 'supertest';
+
 const url = process.env.VUE_APP_API_URL;
+const request = supertest(url);
 
 describe('API GitHub', () => {
   it('Deve ser acessível por variável de ambiente', () => {
     expect(url).toEqual('https://api.github.com');
+  });
+
+  it('Deve responder com sucesso (200) ao ser acessado', async () => {
+    const res = await request.get('/').set('User-agent', 'request');
+
+    expect(res.statusCode).toEqual(200);
   });
 });
