3bc8c912225deeac64c7171275d5b021830adc7c
LP <luizpaulobborges@gmail.com>
2021-08-10 18:01:06 -0300

    added .editorconfig and new rules to eslint

--- git-repos/src/App.tsx
+++ git-repos/src/App.tsx
@@ -1,8 +1,3 @@
-import './App.css';
-
-export function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-};
+export function App(): JSX.Element {
+  return <div className="App" />;
+}
