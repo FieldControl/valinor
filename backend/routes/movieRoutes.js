9c68862c08c474b93c18c4d69263926a86df24e3
javi-domi <javdominguez@icloud.com>
2021-10-20 18:23:38 -0300

    feat: Rebuild app with Angular

--- backend/routes/movieRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import controller from "../controller/movieController.js";
-const app = express();
-app.use(express.json());
-
-app.get("/api/v1/movie/:title", controller.getMovies);
-app.get("/api/v1/movie/id/:id", controller.getById);
-
-export { app as movieRouter };
