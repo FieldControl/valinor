d021667cd0ad82b37f0e24d9b85536cf3a0e130f
javi-domi <javdominguez@icloud.com>
2021-10-21 09:04:47 -0300

    feat: sharing data between components

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RepositoriesService } from './services/repositories.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  searchTerm!: string;
+  repositories!: [];
+
+  constructor(private repositoriesService: RepositoriesService) {}
+
+  searchRepositories(searchTerm: string) {
+    this.searchTerm = searchTerm;
+    console.log(searchTerm);
+    this.repositoriesService
+      .getReposOnSearch(searchTerm)
+      .subscribe((res: any) => {
+        console.log(res.items);
+        this.repositories = res.items;
+        return res;
+      });
+  }
+
+  ngOnInit(): void {}
 }
--- client/src/app/search-bar/search-bar.component.ts
+++ client/src/app/search-bar/search-bar.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { RepositoriesService } from '../services/repositories.service';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search-bar',
@@ -8,24 +6,13 @@ import { RepositoriesService } from '../services/repositories.service';
   styleUrls: ['./search-bar.component.css'],
 })
 export class SearchBarComponent implements OnInit {
-  constructor(private repositoriesService: RepositoriesService) {}
+  constructor() {}
 
-  searchForm: any;
-  searchResult: any;
+  @Output() eventSearch = new EventEmitter<string>();
 
-  ngOnInit(): void {
-    this.searchForm = new FormGroup({
-      search: new FormControl(''),
-    });
-  }
+  ngOnInit(): void {}
 
-  onSubmit(): void {
-    console.log(this.searchForm.value.search);
-    this.repositoriesService
-      .getReposOnSearch(this.searchForm.value.search)
-      .subscribe((res) => {
-        console.log(res);
-        return (this.searchResult = res);
-      });
+  newSearch(value: string) {
+    this.eventSearch.emit(value);
   }
 }
