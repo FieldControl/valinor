6e7c41931ccde107ba1cc8032814fc10e47e0771
Raphael Oliveira dos Santos <raphos27@gmail.com>
2021-11-12 12:32:12 -0300

    inicio da implemetação do componente de busca

--- angular-search-app/src/app/search/search.component.ts
+++ angular-search-app/src/app/search/search.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from "@angular/forms";
+
+import { Subject, throwError, of } from 'rxjs';
+import { map, debounceTime, distinctUntilChanged, switchMap, catchError, retryWhen, retry } from "rxjs/operators";
+import {SearchService} from "../search.service";
+
 
 @Component({
   selector: 'app-search',
@@ -6,6 +12,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  public loading: boolean;
+  public searchTerm = new Subject<string>();
+  public baseUrl = "https://api.github.com/search/repositories";
+  public searchResults: any;
+  public paginationElements: any;
+  public errorMessage: any;
+  public page:any;
 
   constructor() { }
 
