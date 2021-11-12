d7f4416a2e7f96a4992a0a06899f61c244722dc3
Raphael Oliveira dos Santos <raphos27@gmail.com>
2021-11-12 12:28:42 -0300

    Criação da rota

--- angular-search-app/src/app/app-routing.module.ts
+++ angular-search-app/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {Routes, RouterModule} from "@angular/router";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", loadChildren: () => import("./search/search.module").then(m => m.SearchModule)}
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: "enabled"})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
