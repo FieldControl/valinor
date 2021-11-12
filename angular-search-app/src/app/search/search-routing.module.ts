dcb4a459ac2697891a0a0978ed7cfd3dda877eae
Raphael Oliveira dos Santos <raphos27@gmail.com>
2021-11-12 12:22:31 -0300

    Criação e import do modulo search-routing

---
+++ angular-search-app/src/app/search/search-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import {SearchComponent} from "./search.component";
+
+const routes: Routes = [
+  { path: '', component: SearchComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class SearchRoutingModule { }
--- angular-search-app/src/app/search/search.module.ts
+++ angular-search-app/src/app/search/search.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import {CommonModule} from "@angular/common";
-
-
-import { SearchComponent } from './search.component';
 import { ReactiveFormsModule } from '@angular/forms';
+
+import { SearchRoutingModule } from './search-routing.module';
+import { SearchComponent } from './search.component';
+
 import { NgxPaginationModule } from 'ngx-pagination';
 
 @NgModule({
   imports: [
+   SearchRoutingModule,
    ReactiveFormsModule,
    CommonModule,
    NgxPaginationModule
