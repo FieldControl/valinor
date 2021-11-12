a968ac66821d62b973822ca1e17e2ee337e1c2f0
Raphael Oliveira dos Santos <raphos27@gmail.com>
2021-11-12 12:34:34 -0300

    finalização do componente da busca

--- angular-search-app/src/app/search/search.component.ts
+++ angular-search-app/src/app/search/search.component.ts
@@ -20,7 +20,37 @@ export class SearchComponent implements OnInit {
   public errorMessage: any;
   public page:any;
 
-  constructor() { }
+  constructor(private searchService: SearchService) { }
+
+  public searchForm = new FormGroup({
+    search: new FormControl('', Validators.required),
+  });
+  public search(){
+    this.searchTerm.pipe(
+      map((e: any) => {
+        console.log(e.target.value);
+        return e.target.value
+      }),
+      debounceTime(400),
+      distinctUntilChanged(),
+      switchMap(term => {
+        this.loading = true;
+        return this.searchService._searchEntries(term)
+      }),
+      catchError((e) => {
+        //Lida com o erro e o retona;
+        console.log(e)
+        this.loading = false;
+        this.errorMessage = e.message;
+        return throwError(e);
+      }),
+    ).subscribe(v => {
+        this.loading = false;
+        //Retona os resultados e o adiciona no modulo de paginação;
+        this.searchResults = v;
+        this.paginationElements = this.searchResults;
+    })
+  }
 
   ngOnInit() {
   }
