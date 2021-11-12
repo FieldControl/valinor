a11971050903741cb4e59cb74488c0ad3661cf5e
Raphael Oliveira dos Santos <raphos27@gmail.com>
2021-11-12 12:16:02 -0300

    Criação do componente search

--- angular-search-app/src/app/app.module.ts
+++ angular-search-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
