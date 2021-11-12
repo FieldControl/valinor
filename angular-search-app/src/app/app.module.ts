e23a6951633b5d22260b7fc49e0efea12781a240
Raphael Oliveira dos Santos <raphos27@gmail.com>
2021-11-12 12:14:46 -0300

    Setup do modulo HttpClient

--- angular-search-app/src/app/app.module.ts
+++ angular-search-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
