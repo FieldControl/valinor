62ca5a819dd114395650821f30c4955f43a172c5
MateusAlvesP <mateusp2000@hotmail.com>
2021-11-21 19:24:12 -0300

    Barra de pesquisa, loading e integração com api

---
+++ desafio-fieldcontrol/src/app/app.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+type Result = {
+  total_count: number,
+  incomplete_results: boolean,
+  items: Array<any>;
+};
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+
+  url: string = "https://api.github.com/search/repositories?q=";
+  
+  usuarioPesquisou: boolean = false;
+  loading: boolean = false;
+
+  result: Result = {total_count: 0, incomplete_results: false, items: []};
+
+  constructor(private http: HttpClient){}
+
+  title = 'desafio-fieldcontrol';
+
+  onEnter(value: string): void{
+
+    if(value.length > 0){
+      this.loading = true;
+
+      this.http.get<Result>(this.url + value).subscribe(data => {
+        console.log(data);
+        this.result = data;
+        this.usuarioPesquisou = true;
+        this.loading = false;
+      });
+    }
+    else{
+      this.resetSearch();
+    }
+  }
+
+  resetSearch(): void{
+    this.loading = true;
+    window.location.reload();
+    this.loading = false;
+  }
+
+   numberWithPoints(n: number){
+    return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+   }
+
+}
