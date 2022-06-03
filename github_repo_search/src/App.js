8885923882075a0341d0a9c242283157451c5a43
Bryan Fernandes <bryanfernandes8@gmail.com>
2022-06-03 20:22:37 -0300

    incluindo requisicoes de pesquisa de repositorios

--- github_repo_search/src/App.js
+++ github_repo_search/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import {getRepositories, getTopicDescription} from './requests/Requests'
 
 function App() {
+  
+  const [queryString, setQuerystring] = useState('')
+  const [searchRepoResults, setSearchRepoResults] = useState([])
+  const [searchRepoResultsError, setSearchRepoResultsError] = useState([])
+  const [searchRepoResultsDescription, setSearchRepoResultsDescription] = useState('')
+  const [listaDePaginas, setListaDePaginas] = useState([])
+  const [paginaAtual, setPaginaAtual] = useState([])
+
+  
+  const onChangeInputSearchRepo = (event)=>{
+    setQuerystring(event.target.value)
+  }
+  
+  const onClickButtonSearchRepo = ()=>{
+    getRepositories(queryString, "", "", 2, 15).then((response)=>{
+      setSearchRepoResults(response.items)
+    }).catch((error)=>{
+      setSearchRepoResultsError('não foi possível fazer a pesquisa, tente novamente em instantes')
+    })
+    
+    getTopicDescription(queryString).then((response)=>{
+      setSearchRepoResultsDescription(response.items[0].short_description)
+    })
+  }
+
+
+  const searchRepoResultsMap = searchRepoResults.map((repo)=>{
+    return <div key={repo.id}>{repo.full_name}</div>
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <h1>repositories</h1>
+      <div>
+      <input placeholder='buscar repositorio' 
+      value={queryString} 
+      onChange={onChangeInputSearchRepo} /> 
+      <button onClick={onClickButtonSearchRepo} >search</button>
+      </div>
+
+      {searchRepoResultsDescription && searchRepoResultsDescription}
+      {searchRepoResults && searchRepoResultsMap}
     </div>
   );
 }
---
+++ github_repo_search/src/requests/Requests.js
@@ -0,0 +1,29 @@
+import { Octokit } from '@octokit/core'
+
+const access_key = "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"
+
+const octokit = new Octokit({
+    auth: access_key
+  })
+  
+  //busca de repositorios
+  export const getRepositories = async (qString , sort, order, page, per_page)=>{
+     const result = await octokit.request(`GET /search/repositories?q=${qString}`, {
+         sort: sort || "best match",
+         order: order || "desc",
+         per_page: per_page || 10,
+        page: page,
+     })
+
+     return result.data
+  }
+
+  //busca de descrição de tópico
+  export const getTopicDescription = async (qString, page, per_page)=>{
+     const result = await octokit.request(`GET /search/topics?q=${qString}`, {
+         page: page || 1,
+         per_page: per_page || 1,
+     })
+
+     return result.data
+  }
