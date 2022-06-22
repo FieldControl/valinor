1c42cc38e70f45c5570295db7bad7799c81a7836
diegofreitas50 <diegofreitas50@hotmail.com>
2022-06-22 15:04:16 -0300

    feat: initial configs and connect database

---
+++ src/index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDatabase = require("./database/database");
+const userRoute = require("./users/user.route");
+const authRoute = require("./auth/auth.route");
+
+const port = process.env.PORT || 3001;
+const app = express();
+
+connectDatabase();
+app.use(cors());
+app.use(express.json());
+
+app.use("/user", userRoute);
+app.use("/auth", authRoute);
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
