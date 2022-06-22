42bd03251868f65943ea58bd804288048ad0c55e
diegofreitas50 <diegofreitas50@hotmail.com>
2022-06-22 15:21:23 -0300

    feat: user documents and add user model

---
+++ src/users/User.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowecase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    avatar: {
+        type: String,
+        required: true,
+    },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
