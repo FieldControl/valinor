82f4002717c2e4c3b0a15a7bbc0f951200dc2fa6
Diego Freitas dos Santos <diegofreitas50@hotmail.com>
2022-06-23 20:59:52 -0300

    Update users.route.js

--- src/users/users.route.js
+++ src/users/users.route.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const userController = require("./users.controller");
 
-router.post("/", userController.createUserController);
+router.post("/create", userController.createUserController);
 router.get("/", userController.findAllUserController);
 
 module.exports = router;
