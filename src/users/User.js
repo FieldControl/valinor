c18353abd4c61786d593fee54d8d5fe0449d40b6
diegofreitas50 <diegofreitas50@hotmail.com>
2022-06-23 00:58:04 -0300

    feat: bcrypt

--- src/users/User.js
+++ src/users/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -25,6 +26,11 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+});
+
+UserSchema.pre("save", async function (next) {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
 });
 
 const User = mongoose.model("User", UserSchema);
