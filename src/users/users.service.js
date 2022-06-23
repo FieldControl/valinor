c6bdb044bcd16be0a5f52882c43ae5bd116f7394
diegofreitas50 <diegofreitas50@hotmail.com>
2022-06-23 13:43:33 -0300

    feat: auth middleware; chore: user service

--- src/users/users.service.js
+++ src/users/users.service.js
@@ -6,8 +6,11 @@ const createUserService = (body) => User.create(body);
 
 const findAllUserService = () => User.find();
 
+const findByIdUserService = (idUser) => User.findById(idUser);
+
 module.exports = {
-    findByEmailUserService,
-    createUserService,
-    findAllUserService,
-  };
+  findByEmailUserService,
+  createUserService,
+  findAllUserService,
+  findByIdUserService,
+};
