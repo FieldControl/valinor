cef8365c7765b9e4241b2ce4205368a2f1e0f084
LucianoRib5 <luciano.eng.soft@outlook.com>
2022-06-25 14:05:15 -0300

    Creating service to instantiate Axios

---
+++ src/services/apiRequestHttp.tsx
@@ -0,0 +1,7 @@
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
+
+const config: AxiosRequestConfig = {
+    baseURL: "https://api.github.com/search/"
+};
+
+export const client: AxiosInstance = axios.create(config);
