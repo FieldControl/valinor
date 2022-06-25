9621995c2c38a812a8b48e17b8906a5cc53ea311
LucianoRib5 <luciano.eng.soft@outlook.com>
2022-06-25 17:05:16 -0300

    Adding HomePage Component
    Performed the migration of the App file code
    to the index file of the home folder, the component
    HomePage is called in the App provisionally.

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,9 @@
-import { useState, useEffect } from "react";
-import { Repositories } from "./model/repositories";
-import * as services from "./services/apiRequestHttp";
+import { HomePage } from "./pages/home";
 
 export const App: React.FC = () => {
-
-  const [repositories, setRepositories] = useState<[Repositories]>();
-
-  const repositorie: string = "node";
-
-  const getRepositories = (): void => {
-    services.client.get(`/repositories?q=${repositorie}`)
-      .then(res => setRepositories(res.data))
-      .catch(err => console.log(err.response.data));
-  };
-
-  useEffect(() => {
-    getRepositories();
-  }, []);
-
-  console.log(repositories);
-
   return (
     <div>
-      <h1> Deus seja louvado! </h1>
+      <HomePage/>
     </div>
   );
 };
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import { Repositories } from "../../model/repositories";
+import * as services from "../../services/apiRequestHttp";
+
+export const HomePage: React.FC = () => {
+
+    const [repositories, setRepositories] = useState<[Repositories]>();
+
+    const repositorie: string = "node";
+
+    const getRepositories = (): void => {
+        services.client.get(`/repositories?q=${repositorie}`)
+            .then(res => setRepositories(res.data))
+            .catch(err => console.log(err.response.data));
+    };
+
+    useEffect(() => {
+        getRepositories();
+    }, []);
+
+    console.log(repositories);
+    return (
+        <div>
+            <h1> Deus seja louvado!!! </h1>
+        </div>
+    );
+};
