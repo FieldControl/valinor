867c1cd868688440b8b691b7ea0e7a9cadb0979c
LucianoRib5 <luciano.eng.soft@outlook.com>
2022-06-25 14:23:20 -0300

    Creating function to get all repositories

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { useState, useEffect } from "react";
+import * as services from "./services/apiRequestHttp";
+
 export const App = () => {
-  return(
+
+  const [repositories, setRepositories] = useState([]);
+
+  const repositorie = "node";
+
+  const getRepositories = () => {
+    services.client.get(`/repositories?q=${repositorie}`)
+    .then(res => setRepositories(res.data))
+    .catch(err => console.log(err.response.data));
+  };
+
+  useEffect(() => {
+    getRepositories();
+  }, []);
+
+  console.log(repositories);
+
+  return (
     <div>
       <h1> Deus seja louvado! </h1>
     </div>
