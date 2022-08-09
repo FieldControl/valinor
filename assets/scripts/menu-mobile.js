880bf274ea347737b06cc47b8009a915b2311f19
ThesllaDev <theslladev@gmail.com>
2022-08-08 22:54:55 -0300

    🔥Chore: removing all old unnecessary files

--- assets/scripts/menu-mobile.js
+++
@@ -1,7 +0,0 @@
-const iconMenu = document.querySelector('.icon-menu');
-const navMenuList = document.querySelector('.nav-menu__list');
-
-iconMenu.addEventListener('click', () => {
-    iconMenu.classList.toggle('change');
-    navMenuList.classList.toggle('active');
-});
--- assets/scripts/nav-scrolled.js
+++
@@ -1,9 +0,0 @@
-const navMenu = document.querySelector('.nav-menu');
-
-const scrolledEffect = () => {
-    window.scrollY > 50 ?
-        navMenu.classList.add('scrolled') : navMenu.classList.remove('scrolled');
-}
-
-window.onscroll = scrolledEffect;
-window.onload = scrolledEffect;
