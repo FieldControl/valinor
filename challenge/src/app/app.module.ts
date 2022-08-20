c2166e457cc64501a5e4498be260a570030e3c1a
Vitor-Kemuel <kemuvitor@gmail.com>
2022-08-20 12:08:02 -0300

    Adicionando navbar

--- challenge/src/app/app.module.ts
+++ challenge/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ challenge/src/app/core/core.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from "@angular/core";
+import { NavBarComponent } from "./component/nav-bar/nav-bar.component";
+
+@NgModule({
+  declarations: [NavBarComponent],
+  exports: [NavBarComponent],
+})
+export class CoreModule { }
