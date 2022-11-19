18a7d39f37b22abf1f0e68bce51a485b79ad36b5
Adalberto R. Teixeira <adalbertoart@gmail.com>
2022-11-19 19:52:37 -0300

    Primeiro commit

---
+++ components/Header/index.tsx
@@ -0,0 +1,7 @@
+const Header = () => {
+    return (
+        <header><div>MARVEL</div><nav></nav></header>
+    )
+}
+
+export default Header;
---
+++ pages/home/index.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+    
+}
+
+export default Home;
