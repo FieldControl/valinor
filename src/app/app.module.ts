2dde69301e7dff7796ce926cf14776c4b56f078f
leonardo-gomes03 <leogomes.pereira03@gmail.com>
2023-02-15 08:25:06 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AgentsCardComponent } from './components/agents-card/agents-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgentsCardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/agents-card/agents-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agents-card',
+  templateUrl: './agents-card.component.html',
+  styleUrls: ['./agents-card.component.scss']
+})
+export class AgentsCardComponent {
+
+}
