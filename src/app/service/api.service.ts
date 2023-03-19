6d507ee44b325082f939daa34aa9f2ac70d56863
Wellington Fujiwara <92489038+WellingtonFujiwara@users.noreply.github.com>
2023-03-19 15:29:26 -0300

    Update service

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -10,7 +10,7 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   public getRepositories(query: any): Observable<any>{
-    const myToken = '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';
+    const myToken = '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';
     const headers = new HttpHeaders({ Authorization: `Bearer ${myToken}` });
     const url = `https://api.github.com/search/repositories?q=${query}`;
 
