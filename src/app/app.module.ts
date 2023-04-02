6807e14d61d6e43b4fa8c56abf50d6a42a28188a
Arthur De Bellis <arthur.bellis@ufv.br>
2023-04-02 12:53:46 -0300

    chore: Organiza estrutura de pastas do projeto
    Este commit organiza a estrutura de pastas do projeto.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RepositoriesListComponent } from './pages/github-search/repositories-list/repositories-list.component';
+import { RepositorieIssuesComponent } from './pages/github-search/repositorie-issues/repositorie-issues.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    RepositoriesListComponent,
+    RepositorieIssuesComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/github-search/repositorie-issues/repositorie-issues.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-repositorie-issues',
+  templateUrl: './repositorie-issues.component.html',
+  styleUrls: ['./repositorie-issues.component.scss']
+})
+export class RepositorieIssuesComponent {
+
+}
