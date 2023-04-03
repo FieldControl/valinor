fbccc53c43efc21cbc42fce722e1c75fab5e3741
jean <jeanpauloriopreto.code@gmail.com>
2023-04-02 22:28:44 -0300

    desafio: deu um pouco de trabalho para configurar o tema

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Marvel';
+
+  DarkTheme = false;
+
+  constructor() {
+    this.applyTheme();
+  }
+
+  toggleTheme() {
+    this.DarkTheme = !this.DarkTheme;
+    this.applyTheme();
+  }
+
+  applyTheme() {
+    // Seleciona o elemento img com a classe theme-icon
+    const themeIcon = document.querySelector('img.theme-icon');
+
+    if (this.DarkTheme) {
+      document.body.classList.add('dark-theme');
+      document.body.classList.remove('light-theme');
+      // Adiciona a classe rotate ao elemento
+      if (themeIcon)
+        themeIcon.classList.add('rotate');
+    } else {
+      document.body.classList.add('light-theme');
+      document.body.classList.remove('dark-theme');
+      // remove a classe rotate ao elemento
+      if (themeIcon)
+        themeIcon.classList.remove('rotate');
+    }
+  }
 }
