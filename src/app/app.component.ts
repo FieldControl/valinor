9c02e1121c654f63568d90b72e8be55fec0a5f9b
jean <jeanpauloriopreto.code@gmail.com>
2023-04-03 00:48:58 -0300

    melhorias no estilo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Marvel';
@@ -27,14 +27,12 @@ export class AppComponent {
       document.body.classList.add('dark-theme');
       document.body.classList.remove('light-theme');
       // Adiciona a classe rotate ao elemento
-      if (themeIcon)
-        themeIcon.classList.add('rotate');
+      if (themeIcon) themeIcon.classList.add('rotate');
     } else {
       document.body.classList.add('light-theme');
       document.body.classList.remove('dark-theme');
       // remove a classe rotate ao elemento
-      if (themeIcon)
-        themeIcon.classList.remove('rotate');
+      if (themeIcon) themeIcon.classList.remove('rotate');
     }
   }
 }
