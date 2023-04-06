811685a431acb6d83ec460da15cb0b9ef22b5cec
matheusalmeidadev <matheusenrico01@gmail.com>
2023-04-06 16:48:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let result = document.getElementById("result");
 
 let getMovie = () => {
     let movieName = movieNameRef.value;
-    let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
+    let url = `https://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
 
     if (movieName.length <= 0) {
         result.innerHTML = `<h3 class="msg">Insira o nome do filme</h3>`;
