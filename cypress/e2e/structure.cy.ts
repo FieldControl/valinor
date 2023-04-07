cdaca65cb1433608d3dabbe8ce4fd89bcef93281
jean <jeanpauloriopreto.code@gmail.com>
2023-04-06 23:13:20 -0300

    melhorando testes e2e

--- cypress/e2e/structure.cy.ts
+++ cypress/e2e/structure.cy.ts
@@ -3,4 +3,16 @@ describe('App', () => {
     cy.visit('/');
   });
 
+  /* Verifica se o botão de DarkMode está alterando a variável do DarkTheme */
+  it('should toggle DarkTheme variable when toggleTheme is called', () => {
+    cy.window()
+      .its('appComponent')
+      .then((appComponent) => {
+        expect(appComponent.DarkTheme).to.be.false;
+        appComponent.toggleTheme();
+        expect(appComponent.DarkTheme).to.be.true;
+        appComponent.toggleTheme();
+        expect(appComponent.DarkTheme).to.be.false;
+      });
+  });
 });
