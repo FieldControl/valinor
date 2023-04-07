e06301b78a519d29f3f4b895330ad722ff409fa5
jean <jeanpauloriopreto.code@gmail.com>
2023-04-07 00:14:08 -0300

    aplicando dark-theme em componentes que não são através do "theme-switcher"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent {
   title = 'Marvel';
 
   DarkTheme = false;
-  
+
   constructor() {
     (window as any).appComponent = this;
     this.applyTheme();
@@ -23,17 +23,30 @@ export class AppComponent {
   applyTheme() {
     // Seleciona o elemento img com a classe theme-icon
     const themeIcon = document.querySelector('img.theme-icon');
+    const components = document.querySelectorAll('.theme-switcher');
 
     if (this.DarkTheme) {
       document.body.classList.add('dark-theme');
       document.body.classList.remove('light-theme');
       // Adiciona a classe rotate ao elemento
       if (themeIcon) themeIcon.classList.add('rotate');
+      components.forEach((component) => {
+        component.classList.add('dark-theme');
+        component.querySelectorAll('*').forEach((child) => {
+          child.classList.add('dark-theme');
+        });
+      });
     } else {
       document.body.classList.add('light-theme');
       document.body.classList.remove('dark-theme');
       // remove a classe rotate ao elemento
       if (themeIcon) themeIcon.classList.remove('rotate');
+      components.forEach((component) => {
+        component.classList.remove('dark-theme');
+        component.querySelectorAll('*').forEach((child) => {
+          child.classList.remove('dark-theme');
+        });
+      });
     }
   }
 }
