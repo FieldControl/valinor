cd05bcb46bcdfa64e47b0e9de364b11d9beb1da2
Fabricio-Lima <fabriciolc61@gmail.com>
2023-05-01 22:47:09 -0300

    feat: model
    adiciona model de Repositories

---
+++ frontend/src/app/core/models/Repositories.ts
@@ -0,0 +1,13 @@
+export interface Repositories {
+  description: string;
+  name: string;
+  url: string;
+  language: string;
+  stargazers_count: number;
+  updated_at: string;
+  html_url: string;
+  owner: {
+    avatar_url: string;
+  };
+  topics: string[];
+}
