cceae6d426184e2553f872b9ac0cb62112bd0bde
FerreiraJoao1996 <jpfreitass@hotmail.com>
2023-05-25 17:42:51 -0300

    desafio field

--- src/app/repositorios/repositorios.component.ts
+++ src/app/repositorios/repositorios.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ProjetosService } from '../repositorios.service';
 
 @Component({
   selector: 'app-repositorios',
@@ -10,14 +11,19 @@ export class RepositoriosComponent implements OnInit {
   resultados: any;
   pagina: number = 1;
   contador: number = 10;
+  numeroDeRepositorios:number | any
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private projetoService: ProjetosService) {}
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
       if (params['resultados']) {
         this.resultados = JSON.parse(params['resultados']);
       }
+    });
+
+    this.projetoService.getProjeto().subscribe(repositorios => {
+      this.numeroDeRepositorios = repositorios.length;
     });
   }
 }
