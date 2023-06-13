1aa1de1a0cf537b5a7df0d212fa6d80ea6ef6013
Amanda Marques <124704133+theamandamarques@users.noreply.github.com>
2023-06-13 17:01:17 -0300

    Add files via upload
    Angular: Optei pelo Angular como framework de desenvolvimento devido à sua ampla adoção na comunidade de desenvolvimento web e à sua capacidade de criar aplicativos de página única (SPAs) de forma escalável e modular. O Angular oferece recursos poderosos, como injeção de dependência, detecção de mudanças e roteamento, que são úteis ao construir aplicativos complexos.
    
    Angular CLI: O Angular CLI é uma ferramenta de linha de comando que simplifica a criação, configuração e compilação de projetos Angular. Ele fornece uma estrutura inicial bem organizada, geração automática de código e uma maneira fácil de executar tarefas de desenvolvimento, como iniciar um servidor de desenvolvimento local e executar testes.
    
    TypeScript: O Angular utiliza o TypeScript como sua linguagem principal. Optei pelo TypeScript devido à sua tipagem estática, que ajuda a detectar erros durante o desenvolvimento e facilita a manutenção do código. Além disso, o TypeScript possui recursos adicionais, como interfaces e classes, que ajudam a organizar e estruturar o código.
    
    HttpClient: O Angular fornece o módulo HttpClient para fazer chamadas HTTP. Usei o HttpClient para fazer a solicitação para a API do GitHub e receber os dados de repositórios. Ele simplifica a comunicação com serviços externos e lida com tarefas comuns, como serialização e desserialização de dados JSON.
    
    MVC (Model-View-Controller): O Angular adota um padrão de design baseado em MVC, onde os componentes operam como controladores que manipulam os dados (model) e atualizam a exibição (view). Isso ajuda a separar as preocupações e tornar o código mais modular, facilitando a manutenção e o teste.
    
    Princípios SOLID: Ao desenvolver o aplicativo, siga os princípios SOLID da engenharia de software. Por exemplo, utilizei a injeção de dependência para garantir que as classes dependam de abstrações em vez de implementações concretas, facilitando a substituição de componentes e facilitando a testabilidade.

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+[x: string]: any;
+previousPage: any;
+search() {
+throw new Error('Method not implemented.');
+}
+  title = 'github-repo-search';
+repositories: any;
+searchQuery: any;
+search_Query: any;
+}
---
+++ src/app/repository-search/repository-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-repository-search',
+  templateUrl: './repository-search.component.html',
+  styleUrls: ['./repository-search.component.css']
+})
+export class RepositorySearchComponent {
+
+}
