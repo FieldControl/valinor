a7a9067af46f79f049fbf4866590dfe520a94ec9
tauk7 <zexcraft2@gmail.com>
2023-06-20 17:07:17 -0300

    Cria as funções pegar_repositorio no RepositorioService para pegar os repositorios buscados com a API e a mudar_nome_repositorio para chamar a pegar_repositorio, tratar e receber os dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { RepositoriosService } from './services/repositorios.service';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +8,28 @@ import { Component, Input } from '@angular/core';
 })
 export class AppComponent {
   nome_repositorio: string = ''
+  repositorios = []
+  mensagem_erro: string = ''
+  pagina: number = 0
 
-  mudar_nome_repositorio(nome_repositorio: string): void{
+  constructor(private repositorioservice: RepositoriosService) {} 
+
+  mudar_nome_repositorio(nome_repositorio: string, pagina: number): void{
     this.nome_repositorio = nome_repositorio
+    this.repositorioservice.pegar_repositorios(this.nome_repositorio, this.pagina).subscribe((resultado) =>{
+      if(typeof resultado === 'string'){
+        this.mensagem_erro = resultado; 
+        this.repositorios = []
+      }else{
+        if(resultado.total_count === 0){
+          this.mensagem_erro = 'Não achamos esse repositório'
+          this.repositorios = []
+        }else{
+          this.repositorios = resultado.items
+          }
+        }
+      })
   }
+
   title = 'valinor';
 }
--- src/app/services/repositorios.service.ts
+++ src/app/services/repositorios.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, pipe, catchError, of } from 'rxjs'
 
 @Injectable({
   providedIn: 'root'
 })
-export class RepositoriosService {
 
-  constructor() { }
+export class RepositoriosService {
+  private url_api = 'https://api.github.com/search/repositories?q='
+
+  constructor(private http: HttpClient) { }
+
+  pegar_repositorios(nome_repositorio: string, pagina: number): Observable<any>{
+    return this.http.get(this.url_api+nome_repositorio+'?page='+pagina+'per_page=10').pipe(
+      catchError((error) => {
+        const mensagem_erro = error.error.message;
+        return of(mensagem_erro)
+      })
+    )
+
+  }
 }
