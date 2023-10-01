352400c926050e8ec20a1c1bb13fcb9e7521aed7
Matheus Gabriel Tatangelo <54727159+matheustatangelo@users.noreply.github.com>
2023-10-01 20:04:51 -0300

    feat: Create server.js for railway deploy

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+const allowCors = (req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*"); // colocar os dominios permitidos | ex: 127.0.0.1:3000
+    
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials, X-Access-Token, X-Key");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS, PATCH");
+    
+    res.header("Access-Control-Allow-Credentials", "true");
+
+  next();
+}
+
+app.use(allowCors);
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + "/dist/lojaonline"));
+
+// servir o index.html do angular
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + "/dist/lojaonline/index.html");
+});
+
+app.listen(port, async() => {
+    console.log(`API Gateway running in port ${port}!`)
+});
