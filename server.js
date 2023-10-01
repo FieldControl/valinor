7821a9ed75e73485d23f2800f1ca85b585075464
tatangelo <mg-tatangelo@hotmail.com.br>
2023-10-01 20:28:13 -0300

    fix(fix a bug in server.js to railway): deploy to cloud for fieldcontrol

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const port = process.env.PORT || 3000;
 
 const allowCors = (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*"); // colocar os dominios permitidos | ex: 127.0.0.1:3000
-    
+
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials, X-Access-Token, X-Key");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS, PATCH");
-    
+
     res.header("Access-Control-Allow-Credentials", "true");
 
   next();
@@ -20,12 +20,12 @@ const allowCors = (req, res, next) => {
 app.use(allowCors);
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(__dirname + "/dist/lojaonline"));
+app.use(express.static(__dirname + "/dist/valinor"));
 
 // servir o index.html do angular
 
 app.get('*', (req, res) => {
-    res.sendFile(__dirname + "/dist/lojaonline/index.html");
+    res.sendFile(__dirname + "/dist/valinor/index.html");
 });
 
 app.listen(port, async() => {
