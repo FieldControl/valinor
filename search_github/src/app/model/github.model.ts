d351395ed63150111d0c90687163243cb452de52
Eduardo <eduardorsimoes97@gmail.com>
2023-10-21 15:18:26 -0300

    :sparkles: feat: estilização dos cards

--- search_github/src/app/model/github.model.ts
+++ search_github/src/app/model/github.model.ts
@@ -1,11 +1,18 @@
 export interface Item {
     id?: number,
     url?: string,
+    html_url: string,
     full_name?: string,
     description?: string
     language?: string,
     topics?: string[],
-    updated_at?: string
+    updated_at?: string,
+    owner?: {
+        avatar_url?: string
+    },
+    stargazers_count?: number,
+    open_issues?: number,
+    watchers?: number
 }
 
 export interface Data {
