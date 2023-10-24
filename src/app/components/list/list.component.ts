ccea2c792c8cee322b0602861174806e0a73ff6f
leandroJMartin <leandro.dev@outlook.com.br>
2023-10-24 13:14:07 -0300

    feat: created list component

---
+++ src/app/components/list/list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+}
