4b8a3450f4e9e7f71ac5a6077b2910eb4e8a8a85
leandroJMartin <leandro.dev@outlook.com.br>
2023-10-25 15:26:26 -0300

    feat: create Card componente

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,11 @@
+import { Component, NgModule } from '@angular/core';
+import { LucideAngularModule, File, Home, Menu, UserCheck } from 'lucide-angular';
+
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css'],
+})
+
+export class CardComponent { }
