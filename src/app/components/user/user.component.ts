a2894a03d5a5874e3d2da26a68d5ea90369852ca
leandroJMartin <leandro.dev@outlook.com.br>
2023-10-26 17:40:05 -0300

    feat: create User component

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent {
+  @Input() data: any = '';
+
+}
