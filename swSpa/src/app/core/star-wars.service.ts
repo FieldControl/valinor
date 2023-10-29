4abb930ecd6eea972d570428fab624856887358b
renan-augusto <renan_acm1992@hotmail.com>
2023-10-29 13:18:55 -0300

    fix: star wars service fixed

--- swSpa/src/app/core/star-wars.service.ts
+++ swSpa/src/app/core/star-wars.service.ts
@@ -2,10 +2,12 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { People } from '../models/people.model';
+import { ResultWapper } from './common';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class StarWarsService {
 
   private baseUrl = environment.api
@@ -13,14 +15,14 @@ export class StarWarsService {
   constructor(private http: HttpClient) { }
 
   getPeople() {
-    return this.http.get<People[]>(`${this.baseUrl}people/`);
+    return this.http.get<ResultWapper<People>>(`${this.baseUrl}people/`);
   }
 
   searchPeople(query: string) {
-    return this.http.get<People[]>(`${this.baseUrl}people/?search=${query}`);
+    return this.http.get<ResultWapper<People>>(`${this.baseUrl}people/?search=${query}`);
   }
 
   getPagination(page: any) {
-    return this.http.get<People[]>(`${this.baseUrl}people/?page=${page}`);
+    return this.http.get<ResultWapper<People>>(`${this.baseUrl}people/?page=${page}`);
   }
 }
