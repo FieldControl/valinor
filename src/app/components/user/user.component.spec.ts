65693193c4f38a130cba8406fe5ff1707c42362d
leandroJMartin <leandro.dev@outlook.com.br>
2023-11-10 17:01:22 -0300

    feat: adjust user component

--- src/app/components/user/user.component.spec.ts
+++ src/app/components/user/user.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { UserComponent } from './user.component';
 
 describe('UserComponent', () => {
+
   let component: UserComponent;
   let fixture: ComponentFixture<UserComponent>;
 
