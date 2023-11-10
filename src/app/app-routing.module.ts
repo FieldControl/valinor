b0288c30dd542d2b2688db0ac756cf1c66e39a02
leandroJMartin <leandro.dev@outlook.com.br>
2023-11-10 16:40:34 -0300

    feat: inserir componente na rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './components/list/list.component';
+import { UserComponent } from './components/user/user.component';
+import { RepositoryComponent } from './components/repository/repository.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: ListComponent }, { path: 'usuario/:name', component: UserComponent }, { path: ':name/:repo', component: RepositoryComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
