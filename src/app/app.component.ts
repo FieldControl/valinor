04387e60c8f33a4dc2e750b31f9d6fc6c9f1bfaa
leandroJMartin <leandro.dev@outlook.com.br>
2023-11-10 16:50:24 -0300

    feat: Inserting and configuring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Location } from "@angular/common";
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
-  title = 'search-repo-github';
+export class AppComponent implements OnInit {
+
+  url: string = '';
+  constructor(location: Location) {
+    this.url = location.path();
+  }
+
+  ngOnInit(): void { }
+
 }
