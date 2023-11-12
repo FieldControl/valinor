8c583be85210db6a2887c8aae1547756fb37dd9f
ShadicGamesBr <144256901+FelipeKauanG@users.noreply.github.com>
2023-11-12 11:25:35 -0300

    v1

--- GitHub/src/App.jsx
+++ GitHub/src/App.jsx
@@ -1,35 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  var [name, setName] = useState()
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1 className='text text-primary'>GitHub Repositórios</h1>
+      <input type="text" className='form-control' placeholder='' id="" />
+      <input type="submit" className='btn btn-primary' value="Pesquisar" />
+
     </>
   )
 }
-
 export default App
