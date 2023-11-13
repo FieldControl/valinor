85778fa042121f5ac3bbaee35be40ee0c9120732
leandroJMartin <leandro.dev@outlook.com.br>
2023-11-13 09:35:08 -0300

    raw: delete api key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   GITHUB_API_URL: 'https://api.github.com',
-  GITHUB_API_KEY: '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',
+  GITHUB_API_KEY: '',
 };
