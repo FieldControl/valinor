30588db01cbdf9b1ff682e394a59811707941705
Felipe Kauan <144256901+FelipeKauanG@users.noreply.github.com>
2023-11-14 14:36:11 -0300

    v2

--- GitHub/src/App.jsx
+++ GitHub/src/App.jsx
@@ -32,11 +32,26 @@ function App() {
         }
 
         return response.json()
+        
       })
+        
       .then(data => {
-        console.log("https://api.github.com/search/repositories?q=" + originalNome + "&page=" + pag)
+        //console.log("https://api.github.com/search/repositories?q=" + originalNome + "&page=" + pag)
         var container = document.getElementById("container")
-        container.innerHTML = ""
+        //var divRes = document.getElementById("resultados")
+        
+        //res.innerHTML = ""
+        //container.innerHTML = ""
+        //console.log(data)
+        //var resultado = data.total_count
+        
+        
+        
+        //res.innerHTML = `${resultado} resultados`
+        
+        //window.alert(resultado)
+        
+
         for (let i = 0; i < 30; i++) {
           var nomeCompleto = data.items[i].full_name
           var desc = data.items[i].description
@@ -44,20 +59,29 @@ function App() {
             desc = "Sem descrição"
           }
 
+
+          
+
           var forks = data.items[i].forks_count
           var estrelas = data.items[i].stargazers_count
           var upload = data.items[i].updated_at
           var foto = data.items[i].owner.avatar_url
+          var link = data.items[i].html_url
           var novaDiv = document.createElement("div")
+          //var res = document.createElement("div")
 
-
-          novaDiv.innerHTML = `<img src="${foto}"/>  <span><p class='btn btn-success'>${nomeCompleto}</p> <br> <p class='btn btn-primary'>${desc}</p><br> <p class='btn btn-primary'>${upload}</p> <span class="material-symbols-outlined">star</span> <span>${estrelas}</span> </span>`
+          novaDiv.style.border = "2px solid blue"
+          novaDiv.innerHTML = `<img src="${foto}"/>  <span class="material-symbols-outlined">link</span> <span><a target="_blank" href="${link}">
+            <p class='btn btn-success'>${nomeCompleto}</p>
+          </a> <br> <p class='btn btn-primary'>${desc}</p><br> <p class='btn btn-primary'>${upload}</p> <br> <span class="material-symbols-outlined">star</span> <span>${estrelas}</span> watchers <br> <span class="material-symbols-outlined">linked_services</span> ${forks} forks </span> `
           novaDiv.style.color = "black"
-          novaDiv.style.marginTop = "20px"
-          novaDiv.style.backgroundColor = "rgb(0, 0, 0, 0.2)"
+          novaDiv.style.marginTop = "40px"
+          novaDiv.style.backgroundColor = "#272727"
           novaDiv.style.padding = "10px"
           novaDiv.style.borderRadius = "5px"
+
           container.appendChild(novaDiv)
+          //divRes.appendChild(res)
 
 
         }
@@ -66,6 +90,7 @@ function App() {
   return (
     <>
       <div id='app'>
+        <br />
         <button className='btn btn-outline-primary'>Página</button>
         <br />
         <input min={1} defaultValue={1} onClick={handlePag} type="number" className='btn btn-outline-light' placeholder='Página' />
@@ -76,11 +101,14 @@ function App() {
         <form>
           <input type="text" required="True" className='form-control' placeholder='Pesquisar repositórios' onChange={handleChanger} />
           <input type="button" className='btn btn-primary' onClick={handleClick} value="Pesquisar" />
-          
-        </form>
-
+          <br />
+          <div className='btn btn-danger' id="resultados"></div>
         <br />
+        </form>
+        <br />
+        
         <div className='btn btn-primary' id="container"></div>
+        
       </div>
     </>
   )
