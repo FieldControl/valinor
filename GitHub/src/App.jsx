e0b41c0c2cdc12934d5a360abd73e65ebd92361c
Felipe Kauan <144256901+FelipeKauanG@users.noreply.github.com>
2023-11-14 10:19:40 -0300

    v2

--- GitHub/src/App.jsx
+++ GitHub/src/App.jsx
@@ -3,65 +3,68 @@ import './App.css'
 
 function App() {
 
-  
+
   const [nome, setNome] = useState("")
+  const [pag, setPag] = useState("")
 
-  const handChanger = event => {
-    setNome(event.target.value)
 
+
+  const handlePag = event => {
+    setPag(event.target.value)
   }
 
+  const handleChanger = event => {
+    setNome(event.target.value)
+  }
+  
+  var container = document.getElementById("container")
   const handleClick = () => {
     var originalNome = nome.split(" ").join("")
+    console.log(`Pagina: ${pag}`)
 
-    fetch("https://api.github.com/search/repositories?q=" + originalNome)
+    fetch("https://api.github.com/search/repositories?q=" + originalNome + "&page=" + pag)
+      .then(response => {
+        if (!response.ok) {
+          window.alert(`Erro na requisição: ${response.status}`)
+        }
+        return response.json()
+      })
+      .then(data => {
+        console.log("https://api.github.com/search/repositories?q=" + originalNome + "&page=" + pag)
+        //var container = document.getElementById("container")
+        for (let i =0; i < 30; i++){
+           var nomeCompleto = data.items[i].full_name
+           var desc = data.items[i].description
+           var forks = data.items[i].forks_count
+           var estrelas = data.items[i].stargazers_count
+           var upload = data.items[i].updated_at
+           var foto = data.items[i].owner.avatar_url
 
-    .then(response =>{
-      if (!response.ok) {
-        window.alert(`Erro na requisição: ${response.status}`)
-      }
+           console.log(`ID:${i}\nNome: ${nomeCompleto}\nDescrição ${desc}\nForks ${forks}\nUpado em ${upload}\nEstrelas ${estrelas}\nFoto URL: ${foto}`)
+            var novaDiv = document.createElement("div")
+           novaDiv.textContent = `ID:${i}\nNome: ${nomeCompleto}\nDescrição ${desc}\nForks ${forks}\nUpado em ${upload}\nEstrelas ${estrelas}\nFoto URL: ${foto}`
+           novaDiv.style.color = "black"
+            novaDiv.style.border = "2px solid black"
+           container.appendChild(novaDiv)
 
-      return response.json()
-    })
-    .then(data => {
-      console.log(data)
-      var container = document.getElementById("container")
-      var nomeCompleto = data.items[0].full_name
-      var desc = data.items[0].description
-      var forks = data.items[0].forks_count
-      var estrelas = data.items[0].stargazers_count
-      var upload = data.items[0].updated_at
-      var foto = data.items[0].owner.avatar_url
-
-      console.log(`Nome: ${nomeCompleto}\nDescrição ${desc}\nForks ${forks}\nUpado em ${upload}\nEstrelas ${estrelas}\nFoto URL: ${foto}`)
-      window.alert()
-    })
-
+           
+        }
+      })
   }
   return (
     <>
-      <input type="text" placeholder='Página'/>
       <div id='app'>
+        <input min={1} defaultValue={1} onClick={handlePag} type="number" className='btn btn-outline-light' placeholder='Página' />
+
+
         <h1 className='text text-primary'>GitHub Repositórios</h1>
 
-        <input type="text" className='form-control' placeholder='Pesquisar repositórios' onChange={handChanger} />
 
+        <input type="text" className='form-control' placeholder='Pesquisar repositórios' onChange={handleChanger} />
         <input type="button" className='btn btn-primary' onClick={handleClick} value="Pesquisar" />
 
-        <div id="container">
-        Nome: nodejs/node <br />
-        Descrição Node.js JavaScript runtime :sparkles::turtle::rocket::sparkles: <br />
-        Forks 27642 <br />
-        Upado em 2023-11-13T16:19:18Z <br />
-        Estrelas 99333 <br />
-
-        Foto URL: https://avatars.githubusercontent.com/u/9950313?v=4 <br />
-        </div>
-
-
+        <div id="container"></div>
       </div>
-
-
     </>
   )
 }
