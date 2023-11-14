0dad789154be8eab2a2f3ce231eeb0c8da0a9132
Felipe Kauan <144256901+FelipeKauanG@users.noreply.github.com>
2023-11-14 11:41:27 -0300

    v2

--- GitHub/src/App.jsx
+++ GitHub/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChanger = event => {
     setNome(event.target.value)
   }
-  
+
   var container = document.getElementById("container")
   const handleClick = () => {
     var originalNome = nome.split(" ").join("")
@@ -24,46 +24,63 @@ function App() {
 
     fetch("https://api.github.com/search/repositories?q=" + originalNome + "&page=" + pag)
       .then(response => {
-        if (!response.ok) {
-          window.alert(`Erro na requisição: ${response.status}`)
+        if (response.status == "403") {
+          window.alert(`Limite de requisições (30)`)
         }
+        else if (response.status == "422"){
+          window.alert("Por favor, preencha todos os dados")
+        }
+
         return response.json()
       })
       .then(data => {
         console.log("https://api.github.com/search/repositories?q=" + originalNome + "&page=" + pag)
-        //var container = document.getElementById("container")
-        for (let i =0; i < 30; i++){
-           var nomeCompleto = data.items[i].full_name
-           var desc = data.items[i].description
-           var forks = data.items[i].forks_count
-           var estrelas = data.items[i].stargazers_count
-           var upload = data.items[i].updated_at
-           var foto = data.items[i].owner.avatar_url
+        var container = document.getElementById("container")
+        container.innerHTML = ""
+        for (let i = 0; i < 30; i++) {
+          var nomeCompleto = data.items[i].full_name
+          var desc = data.items[i].description
+          if (desc == null){
+            desc = "Sem descrição"
+          }
 
-           console.log(`ID:${i}\nNome: ${nomeCompleto}\nDescrição ${desc}\nForks ${forks}\nUpado em ${upload}\nEstrelas ${estrelas}\nFoto URL: ${foto}`)
-            var novaDiv = document.createElement("div")
-           novaDiv.textContent = `ID:${i}\nNome: ${nomeCompleto}\nDescrição ${desc}\nForks ${forks}\nUpado em ${upload}\nEstrelas ${estrelas}\nFoto URL: ${foto}`
-           novaDiv.style.color = "black"
-            novaDiv.style.border = "2px solid black"
-           container.appendChild(novaDiv)
+          var forks = data.items[i].forks_count
+          var estrelas = data.items[i].stargazers_count
+          var upload = data.items[i].updated_at
+          var foto = data.items[i].owner.avatar_url
+          var novaDiv = document.createElement("div")
 
-           
+
+          novaDiv.innerHTML = `<img src="${foto}"/>  <span><p class='btn btn-success'>${nomeCompleto}</p> <br> <p class='btn btn-primary'>${desc}</p><br> <p class='btn btn-primary'>${upload}</p> <span class="material-symbols-outlined">star</span> <span>${estrelas}</span> </span>`
+          novaDiv.style.color = "black"
+          novaDiv.style.marginTop = "20px"
+          novaDiv.style.backgroundColor = "rgb(0, 0, 0, 0.2)"
+          novaDiv.style.padding = "10px"
+          novaDiv.style.borderRadius = "5px"
+          container.appendChild(novaDiv)
+
+
         }
       })
   }
   return (
     <>
       <div id='app'>
+        <button className='btn btn-outline-primary'>Página</button>
+        <br />
         <input min={1} defaultValue={1} onClick={handlePag} type="number" className='btn btn-outline-light' placeholder='Página' />
-
+        
 
         <h1 className='text text-primary'>GitHub Repositórios</h1>
 
+        <form>
+          <input type="text" required="True" className='form-control' placeholder='Pesquisar repositórios' onChange={handleChanger} />
+          <input type="button" className='btn btn-primary' onClick={handleClick} value="Pesquisar" />
+          
+        </form>
 
-        <input type="text" className='form-control' placeholder='Pesquisar repositórios' onChange={handleChanger} />
-        <input type="button" className='btn btn-primary' onClick={handleClick} value="Pesquisar" />
-
-        <div id="container"></div>
+        <br />
+        <div className='btn btn-primary' id="container"></div>
       </div>
     </>
   )
