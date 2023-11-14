afc4eb0e6562393a81ceaf501451bd4ef143e19a
leandroJMartin <leandro.dev@outlook.com.br>
2023-11-14 11:05:13 -0300

    docs: inserting comment in line

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -18,11 +18,13 @@ export class CardComponent implements OnInit {
 
   ngOnInit(): void {
 
+    /*-- Setting the link to the breadcrumb text --*/
     this.description = this.data.login || 'Usuário: ' + this.data.full_name?.split('/')[0];
     this.urlRepo = this.data.owner ? this.data.owner?.login : this.data.login;
 
   }
 
+  /*-- Function responsible for pagination --*/
   navigateTo(name: string) {
     let path = this.option === 'repositories' ? this.urlRepo : '/usuario/';
     this.router.navigate([path, name]);
