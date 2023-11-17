c42ccd10e5de67962dddbe177774cfed58d95ab0
leandroJMartin <leandro.dev@outlook.com.br>
2023-11-17 16:57:16 -0300

    test: Updating tests

--- src/app/components/search/search.component.spec.ts
+++ src/app/components/search/search.component.spec.ts
@@ -1,10 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { SearchComponent } from './search.component';
+import { GetDataApiGitHub } from '../../services/get-service';
+import { of } from 'rxjs';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
+  let service: GetDataApiGitHub;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -17,5 +19,11 @@ describe('SearchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call search method', () => {
+    spyOn(service, 'getDataByTerm').and.returnValue(of({}));
+    component.search();
+    expect(service.getDataByTerm).toHaveBeenCalled();
   });
 });
