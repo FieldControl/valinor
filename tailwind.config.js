cd8e156c6caf757abac33c335ffff2b00f7fbe3a
Kaio Maciel <kaioreis1153@hotmail.com>
2023-11-21 17:33:31 -0300

    Added Pagination Design

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        montserrat: ['Montserrat']
+      }
+    },
   },
   plugins: [],
 }
