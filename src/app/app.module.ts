fb0a3d0f14aa65fa3505a4afcb224e2d2d322287
Kaio Maciel <kaioreis1153@hotmail.com>
2023-11-21 13:02:41 -0300

    Added Search Return API Items | Improving UI Return

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './pages/search/search.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { SearchComponent } from './pages/search/search.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,28 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 import { Repository } from 'src/app/models/repository';
 import { GithubService } from 'src/app/services/github.service';
+import { emojify } from 'node-emoji';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
 
-  constructor(private githubService : GithubService){}
+export class SearchComponent {
 
-  page : number = 1;
-  perPage : number = 5;
-  search : string = "kaiogotyacodeeee";
 
-  repositories? : Repository;
 
-  ngOnInit(): void {
-    this.githubService.getRepositories(this.page,this.perPage,this.search).subscribe(x =>{
-      if(x.items.length > 0){
-        this.repositories = x;
+  constructor(private githubService: GithubService) { }
+
+  page: number = 1;
+  perPage: number = 5;
+  search?: string;
+  repositories?: Repository;
+
+
+  searchRepo(): void {
+    this.githubService.getRepositories(this.page, this.perPage, this.search).subscribe(root => {
+      if (root.items.length > 0) {
+        this.repositories = root;
       }
     })
-  }  
+  }
+
+  formatDescription(description: string) {
+    return emojify(description);
+  }
 
 }
+
