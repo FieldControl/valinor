5830e58167cbffe79380f4f70c30228ca5d01af9
leandroJMartin <leandro.dev@outlook.com.br>
2023-11-21 17:12:14 -0300

    feat: inserting kEY API into environment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
+const API_KEY = import.meta.env.NG_APP_API_KEY;
+
 export const environment = {
   production: true,
   GITHUB_API_URL: 'https://api.github.com',
-  GITHUB_API_KEY: '',
+  GITHUB_API_KEY: API_KEY,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
-const API_KEY = '';
+const API_KEY = import.meta.env.NG_APP_API_KEY;
 
 export const environment = {
   production: false,
   GITHUB_API_URL: 'https://api.github.com',
-  GITHUB_API_KEY: '',
+  GITHUB_API_KEY: API_KEY,
 };
