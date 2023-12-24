c1e1104e88b5ed0d8f887a102e9670a644222b7c
rebeccaaaaaaaaaaa <rebeccaignacio1996@gmail.com>
2023-12-23 21:00:33 -0300

    refactor: change function format from image component

--- app_react/src/components/Image/index.tsx
+++ app_react/src/components/Image/index.tsx
@@ -1,15 +1,12 @@
-import React from 'react';
-
 interface ImageProps {
   src: string;
   alt?: string;
   className?: string;
 }
 
-const Image: React.FC<ImageProps> = ({ src, alt = '', className = '' }) => {
+export function Image({ src, alt, className }: ImageProps){
   return (
     <img src={src} alt={alt} className={className} />
   );
 }
 
-export default Image;
