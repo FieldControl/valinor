86399b8574fb6b80f302eea1692ff1bfb7b40ec9
rebeccaaaaaaaaaaa <rebeccaignacio1996@gmail.com>
2023-12-23 21:49:04 -0300

    chore: unistall jest and testing library dependencies

--- app_react/src/jest.config.cjs
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-undef */
-module.exports = {
-    testEnvironment: 'jest-environment-jsdom',
-    setupFilesAfterEnv: ['<rootDir>/.jest/setup-tests.js'],
-    moduleNameMapper: {
-      '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/.jest/mocks/fileMock.js',
-      '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
-    },
-  }
