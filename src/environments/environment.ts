6437fd0d944d97caa528f89b19daf94f6c8e5a11
Lucas Dantas <dants.dev@gmail.com>
2024-01-18 12:37:53 -0300

    feat: configuração do ambiente de desenvolvimento com angular 17

--- src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  API_URL: "https://pro-api.coinmarketcap.com/v1/cryptocurrency/trending/latest",
-  API_KEY: "1da791c8-92ac-4017-8748-a08296ebac7c"
-};
