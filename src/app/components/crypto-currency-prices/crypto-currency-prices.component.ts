d1642e6d42ead293816030fbfa93b1b5ed2b392d
Lucas <dants.dev@gmail.com>
2024-01-21 08:55:42 -0300

    feat: adiciona carrousel da lib swaperjs

--- src/app/components/crypto-currency-prices/crypto-currency-prices.component.ts
+++ src/app/components/crypto-currency-prices/crypto-currency-prices.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
-import {MatTableModule} from '@angular/material/table';
+import { CryptoCoin } from '../../models/crypto-coin';
+import { CoinMarketCapAPIService } from '../../services/coin-market-cap-api.service';
 
 @Component({
   selector: 'app-crypto-currency-prices',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [],
   templateUrl: './crypto-currency-prices.component.html',
-  styleUrl: './crypto-currency-prices.component.css'
+  styleUrl: './crypto-currency-prices.component.css',
 })
 export class CryptoCurrencyPricesComponent {
+  cryptos: CryptoCoin | any;
+
+  constructor(private service: CoinMarketCapAPIService) {}
+
+  ngOnInit(): void {
+    this.service.getCryptoData().subscribe({
+      next: (response: CryptoCoin) => {
+        this.cryptos = response.data;
+      },
+    });
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,6 @@
-import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit} from '@angular/core';
+import { Swiper } from 'swiper/types';
 import { CryptoCardComponent } from '../../components/crypto-card/crypto-card.component';
-import { CoinMarketCapAPIService } from '../../services/coin-market-cap-api.service';
-import { CryptoCoin } from '../../models/crypto-coin';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { CryptoCurrencyPricesComponent } from '../../components/crypto-currency-prices/crypto-currency-prices.component';
 
 @Component({
@@ -10,29 +8,17 @@ import { CryptoCurrencyPricesComponent } from '../../components/crypto-currency-
   standalone: true,
   imports: [
     CryptoCardComponent,
-    MatSlideToggleModule,
     CryptoCurrencyPricesComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-
 export class HomeComponent implements OnInit {
   title: string = 'Valinor Coin';
-  subtitle: string = 'Preços de criptomoedas por capitalização de mercado';
-  cryptos: CryptoCoin | any;
+  subtitle: string = 'Preços de criptomoedas por\n capitalização de mercado';
 
-
-  constructor(private service: CoinMarketCapAPIService) {}
 
   ngOnInit(): void {
-    this.service.getCryptoData().subscribe({
-      next: (response: CryptoCoin) => {
-        this.cryptos = response.data;
-      },
-    });
   }
 }
-
-
