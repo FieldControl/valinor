ee0430326a729debb91ccf02b87548669b0f630a
Lucas <dants.dev@gmail.com>
2024-01-24 17:02:45 -0300

    fix: configurações do service

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  API_KEY: 'coinranking9e0071598aca6480e5f810592b78f3598fab35c6c3022ce3',
+  API_URL: 'https://api.coinranking.com/v2',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  API_KEY: process.env["API_KEY"],
+  API_URL: process.env["API_URL"]
+};
