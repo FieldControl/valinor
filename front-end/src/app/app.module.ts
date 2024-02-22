c80bc6a55c369835582b0ec3cc028f53d76d28ef
Igor Augusto Gomes <iagomes95@gmail.com>
2024-02-22 13:56:21 -0300

    refactor: New Component Kanban

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { KanbanComponent } from './component/kanban/kanban.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    KanbanComponent
   ],
   imports: [
     BrowserModule,
---
+++ front-end/src/app/component/kanban/kanban.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kanban',
+  templateUrl: './kanban.component.html',
+  styleUrls: ['./kanban.component.css']
+})
+export class KanbanComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
