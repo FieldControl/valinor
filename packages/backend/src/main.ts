e174acdd90a76b1a4609957e55cda0fb3cc271e5
Lucas Dantas <dants.dev@gmail.com>
2024-03-12 17:05:34 -0300

    fix: configs deploy

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
