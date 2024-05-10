0a882faaed1ffea39518add41a1389d8a935fb60
Lucas Oliveira <lucasantonio97@live.com>
2024-05-10 19:23:16 -0300

    feat: criando colunas e botão para criar os cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './componentes/home/home.component';
 import { NavigationComponent } from './componentes/navigation/navigation.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { HomeComponent } from './componentes/home/home.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomeComponent,NavigationComponent],
+  imports: [RouterOutlet,HomeComponent,NavigationComponent,DragDropModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/componentes/home/home.component.ts
+++ src/app/componentes/home/home.component.ts
@@ -1,12 +1,50 @@
 import { Component } from '@angular/core';
+import {
+  CdkDragDrop,
+  moveItemInArray,
+  transferArrayItem,
+  CdkDrag,
+  CdkDropList
+} from '@angular/cdk/drag-drop';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CdkDropList, CdkDrag],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+
+  addCampo(): void {
+    const userInput = prompt("Digite o nome da venda");
+    if (userInput !== null && userInput !== "") {
+      const novaVenda = document.createElement("h3");
+      novaVenda.classList.add("venda");
+      const texto = document.createTextNode(userInput);
+      novaVenda.appendChild(texto);
+      const card = document.getElementById("card");
+      card?.appendChild(novaVenda);
+    }
+  }
+
+
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+    }
+  }
 
 }
