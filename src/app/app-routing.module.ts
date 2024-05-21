7ab7b4cc31fa96fa6fee02d75fec362d0ea5a236
EstevanVenancio <estevanvenancio12@gmail.com>
2024-05-20 23:52:45 -0300

    kanban

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TodoComponent } from './todo/todo.component';
+
+const routes: Routes = [
+  {
+    path: '', component: TodoComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
