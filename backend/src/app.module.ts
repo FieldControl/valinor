c24dc36098b068fa9ee82ebe19b18df7fd2ceb64
thales-als <thalessouza.dev@gmail.com>
2024-05-23 14:26:58 -0300

    raw: adding local mysql database connections

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,10 +5,30 @@ import { UserModule } from './user/user.module';
 import { BoardModule } from './board/board.module';
 import { SwimlaneModule } from './swimlane/swimlane.module';
 import { CardModule } from './card/card.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Board } from './board/entities/board.entity';
+import { Card } from './card/entities/card.entity';
+import { Swimlane } from './swimlane/entities/swimlane.entity';
+import { User } from './user/entities/user.entity';
 
 @Module({
-  imports: [UserModule, BoardModule, SwimlaneModule, CardModule],
+  imports: [
+    UserModule,
+    BoardModule,
+    SwimlaneModule,
+    CardModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'thales',
+      password: 'password',
+      database: 'kanban_fc',
+      entities: [Board, Card, Swimlane, User],
+      synchronize: process.env.ENV !== 'production',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
