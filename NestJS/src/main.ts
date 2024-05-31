fdec1bfe5aaa1c864ec74e660b3c6f3d35f19ec3
Lucas Oliveira <lucasantonio97@live.com>
2024-05-31 18:47:59 -0300

    feat: vps

--- NestJS/src/main.ts
+++ NestJS/src/main.ts
@@ -1,14 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('/etc/letsencrypt/archive/lucascriado.com/privkey2.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/archive/lucascriado.com/fullchain2.pem')
+  };
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
+
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
-    whitelist:true,
-  })
-);
+    whitelist: true,
+  }));
+
   await app.listen(3001);
+  console.log(`Application is running on: https://localhost:3001/`);
 }
+
 bootstrap();
