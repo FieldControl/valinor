83b8e7c5d37fc21125a7e1e4878a3714564b07f9
Felipe Matheus Mendes Mori <fmatheusog@gmail.com>
2024-08-19 22:43:41 -0300

    feat (server): implement prisma module and service

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 
+import { PrismaModule } from './prisma/prisma.module';
+
 @Module({
-  imports: [],
+  imports: [PrismaModule],
   controllers: [],
   providers: [],
 })
