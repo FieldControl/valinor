e4e44f9305ab84f64ea5d87d26d791568016b875
Felipe Matheus Mendes Mori <fmatheusog@gmail.com>
2024-08-20 15:37:33 -0300

    feat (server): implement validation pipe and add validators into CreateColumnDTO

--- server/src/columns/dto/create-column.dto.ts
+++ server/src/columns/dto/create-column.dto.ts
@@ -1,6 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty } from 'class-validator';
 
 export class CreateColumnDto {
   @ApiProperty()
+  @IsNotEmpty()
   title: string;
 }
--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,7 +14,9 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('swagger', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(3000);
 }
