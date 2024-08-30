15e44519de5823af935855433212ff4b7f828164
Felipe Matheus Mendes Mori <fmatheusog@gmail.com>
2024-08-30 00:44:00 -0300

    feat (web): create angular project and add angular material

---
+++ web/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideAnimationsAsync()],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
