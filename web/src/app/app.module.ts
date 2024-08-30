241481c854eea3193c7b125aab9455e495ce07a8
Felipe Matheus Mendes Mori <fmatheusog@gmail.com>
2024-08-30 00:44:54 -0300

    feat (web): create header component

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule, MatIconModule],
   providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
