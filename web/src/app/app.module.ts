3a04d6287cdda36ebc0958b2193d40e649a3c947
Felipe Matheus Mendes Mori <fmatheusog@gmail.com>
2024-08-30 00:51:16 -0300

    feat (web): create temp home page with all components

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,24 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+// angular material modules
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatButtonModule } from '@angular/material/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// components
 import { HeaderComponent } from './components/header/header.component';
 import { NavigationMenuComponent } from './components/navigation-menu/navigation-menu.component';
 import { TaskContainerComponent } from './components/task-container/task-container.component';
 import { TaskCardComponent } from './components/task-card/task-card.component';
+// pages
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    // components
     HeaderComponent,
     NavigationMenuComponent,
     TaskContainerComponent,
     TaskCardComponent,
+    // pages
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
---
+++ web/src/app/pages/home/home.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {}
