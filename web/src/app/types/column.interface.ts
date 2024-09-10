9d9713ea7173b91444332218c129c1c38c9e2bd9
Felipe Matheus Mendes Mori <fmatheusog@gmail.com>
2024-09-09 21:25:40 -0300

    feat (web): create task and column interfaces

---
+++ web/src/app/types/column.interface.ts
@@ -0,0 +1,6 @@
+export interface Column {
+  id: string;
+  title: string;
+  createdAt: string;
+  updatedAt: string;
+}
---
+++ web/src/app/types/task.interface.ts
@@ -0,0 +1,9 @@
+export interface Task {
+  id: string;
+  title: string;
+  description: string;
+  position: number;
+  createdAt: string;
+  updatedAt: string;
+  columnId: string;
+}
