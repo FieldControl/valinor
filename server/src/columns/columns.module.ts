7c02c12ab8876e02682d1f65e4a6b333793ccfb8
Felipe Matheus Mendes Mori <fmatheusog@gmail.com>
2024-09-22 20:45:47 -0300

    fix (server): correct prisma service import path into columns module

--- server/src/columns/columns.module.ts
+++ server/src/columns/columns.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 
 import { ColumnsService } from './columns.service';
 import { ColumnsController } from './columns.controller';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Module({
   controllers: [ColumnsController],
