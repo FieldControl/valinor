2efaf87ef236dc724a1da7e0afba640645edbbbf
Vitu <vitorcsharp@hotmail.com>
2024-10-15 10:35:57 -0300

    feat: inclusion of bootstrap and material design for our layout, creation of login - registration - home - kanban and guard modules, creation of service

--- frontend/kanbanFrontChallenge/src/app/app.module.ts
+++ frontend/kanbanFrontChallenge/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { KanbanComponent } from './kanban/kanban.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    KanbanComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/kanbanFrontChallenge/src/app/kanban/kanban.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kanban',
+  templateUrl: './kanban.component.html',
+  styleUrls: ['./kanban.component.scss']
+})
+export class KanbanComponent {
+
+}
