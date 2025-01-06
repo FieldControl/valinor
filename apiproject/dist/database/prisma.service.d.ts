17c8456e32a8dbfe758711b65bc11c9fb7edb375
Eric-LDo <121363351+Eric-LDo@users.noreply.github.com>
2025-01-06 16:56:10 -0300

    test of commits

--- apiproject/dist/database/prisma.service.d.ts
+++
@@ -1,6 +0,0 @@
-import { INestApplication, OnModuleInit } from "@nestjs/common";
-import { PrismaClient } from "@prisma/client";
-export declare class PrismaService extends PrismaClient implements OnModuleInit {
-    onModuleInit(): Promise<void>;
-    enableShutdownHooks(app: INestApplication): Promise<void>;
-}
