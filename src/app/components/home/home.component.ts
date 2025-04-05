d56643e3038488d6ee0733ae335e571de51a924a
cristiana <cristiana.luiza@outlook.com>
2025-04-05 12:38:25 -0300

    Ajuta o layout da coluna e dos cards

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import{ColunaComponent} from '../coluna/coluna.component';
+import{NavBarraComponent}from'../navbarra/navBar.component';
+
 
 @Component({
   selector: 'app-home',
-  imports: [CommonModule,ColunaComponent],
+  imports: [CommonModule,ColunaComponent,NavBarraComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
---
+++ src/app/components/navbarra/navBar.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'nav-barra',
+    imports: [CommonModule],
+    templateUrl: './navBar.component.html',
+    styleUrl: './navBar.component.css'
+})
+export class NavBarraComponent {
+   
+}
