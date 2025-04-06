d8e7ebc2e092c378979a4c8ae0c0ed23ab9cb1dd
Matheus Cervantes <matheuscervantes1@gmail.com>
2025-04-06 17:12:19 -0300

    Correções de indentação

--- kanban-backend/src/kanban/entities/CardEntity.ts
+++ kanban-backend/src/kanban/entities/CardEntity.ts
@@ -1,7 +1,13 @@
 // Criando a entidade CardEntity será responsável por representar os cartões do kanban
 // e suas propriedades no banco de dados.
 
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity('card_entity')
 export class CardEntity {
@@ -17,7 +23,7 @@ export class CardEntity {
   @Column({})
   columnId: number; // Pelo id da coluna será detectado a qual coluna o card pertence,
   //  haverá apenas 3 colunas, então não é necessário uma tabela coluna
-  
+
   @CreateDateColumn({ name: 'created_at' })
   createdAt: Date;
 
--- kanban-backend/src/migrations/1743969233462-CreateTablesKanban.ts
+++ kanban-backend/src/migrations/1743969233462-CreateTablesKanban.ts
@@ -1,14 +1,15 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class CreateTablesKanban1743969233462 implements MigrationInterface {
-    name = 'CreateTablesKanban1743969233462'
+  name = 'CreateTablesKanban1743969233462';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "card_entity" ("id" SERIAL NOT NULL, "title" character varying NOT NULL, "description" character varying, "columnId" integer NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_b9a88963999378ac2b88052a3ce" PRIMARY KEY ("id"))`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "card_entity" ("id" SERIAL NOT NULL, "title" character varying NOT NULL, "description" character varying, "columnId" integer NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_b9a88963999378ac2b88052a3ce" PRIMARY KEY ("id"))`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "card_entity"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "card_entity"`);
+  }
 }
