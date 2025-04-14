fd49cb947dc7a3750ca4b97d17fa8a4815cf72ec
Eric <ericld.eld@gmail.com>
2025-04-14 17:12:58 -0300

    start home page

--- taskflow/src/app/modules/pages/home/home.component.ts
+++ taskflow/src/app/modules/pages/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HeaderComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
