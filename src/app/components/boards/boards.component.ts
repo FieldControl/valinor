68b4b115b17718376ab45b5fcd27914792c74878
Pedro Schiavetto <pedro.schiavetto@gmail.com>
2025-04-29 03:42:43 -0300

    atualização dos cards das tasks

--- src/app/components/boards/boards.component.ts
+++ src/app/components/boards/boards.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService, Task } from '../../services/task.service';
-
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 @Component({
   selector: 'app-boards',
-  imports: [],
+  imports: [MatIconModule, MatDividerModule, MatButtonModule, MatMenuModule],
   standalone: true,
   templateUrl: './boards.component.html',
   styleUrl: './boards.component.css'
