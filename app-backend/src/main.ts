96182dafbe5fdfe8188b4881da9adbbc948d14b0
Ederson <edersonh.a.b@gmail.com>
2025-06-12 02:48:03 -0300

    chore: integracao api frontend

--- app-backend/src/main.ts
+++ app-backend/src/main.ts
@@ -5,18 +5,24 @@ import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
   const app: INestApplication = await NestFactory.create(AppModule);
+
+  // 1) validação global
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
+  // 2) habilita CORS para todas origens, métodos e cabeçalhos
+  app.enableCors();
+
+  // 3) swagger
   const config = new DocumentBuilder()
     .setTitle('Kanban API')
     .setDescription('API para gerenciar usuários, colunas e cards')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
-
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
+  // 4) start
   const port = Number(process.env.PORT) || 3000;
   await app.listen(port);
   console.log(`Application running on: http://localhost:${port}`);
