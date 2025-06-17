507433ea57d965b3d7e3624c18206271e9a4b4fc
lucas <lvinicios365@gmail.com>
2025-06-17 11:56:33 -0300

    um errinho mínusculo

--- LucasVinicios/Desafio/kanban-frontend/src/app/app.config.ts
+++ LucasVinicios/Desafio/kanban-frontend/src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS, provideHttpClient } from '@angular/common/http'; 
-import { AuthInterceptor } from './auth/auth.interceptor'; /
+import { AuthInterceptor } from './auth/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
