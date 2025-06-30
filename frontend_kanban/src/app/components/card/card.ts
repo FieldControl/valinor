3f7cfc297bccb55bc9a0bb0630fab7767e5c9ed4
Gustavo <henriqueminghetti@gmail.com>
2025-06-30 13:42:52 -0300

    feat(/components): kanban show columns and cards

--- frontend_kanban/src/app/components/card/card.ts
+++ frontend_kanban/src/app/components/card/card.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CardModel } from '../../models/kanban.model';
 
 @Component({
   selector: 'app-card',
@@ -7,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrl: './card.css'
 })
 export class Card {
+  @Input() cardModel!: CardModel;
+
+constructor() {}
 
 }
