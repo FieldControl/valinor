62ed14708908aa4b6a6cad48d5447a4df0fa7ba0
Gustavo <henriqueminghetti@gmail.com>
2025-07-03 10:44:39 -0300

    refactor(/firebase/firebase.service): Json access changed

--- backend-kanban/src/firebase/firebase.service.ts
+++ backend-kanban/src/firebase/firebase.service.ts
@@ -1,20 +1,30 @@
 import { Injectable, InternalServerErrorException, Logger, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import * as admin from 'firebase-admin';
-import * as path from 'path';
 
 @Injectable()
 export class FirebaseService implements OnModuleInit{
     private readonly logger = new Logger(FirebaseService.name)
     public firestore: admin.firestore.Firestore;
 
+    constructor(private configService: ConfigService) {}
+
+
     onModuleInit() {
         if (!admin.apps.length){
             try{
-                const ServiceAccountPath = path.resolve(__dirname, '../../kanban-9eb1f-firebase-adminsdk-fbsvc-c9c5f73457.json');
-                const ServiceAccount = require(ServiceAccountPath);
+                const serviceAccountJson = this.configService.get<string>('FIREBASE_SERVICE_ACCOUNT_JSON');
+
+                if (!serviceAccountJson) {
+                    throw new Error('FIREBASE_SERVICE_ACCOUNT_JSON environment variable is not set. Cannot initialize Firebase Admin SDK.');
+                }
+                const serviceAccount = JSON.parse(serviceAccountJson);
+                
                 admin.initializeApp({
-                    credential: admin.credential.cert(ServiceAccount),
+                    credential: admin.credential.cert(serviceAccount),
                 });
+
+
                 this.logger.log('FirebaseStarted')
             }catch (error){
                 this.logger.error('FirebaseErrorNotStarted', error.message)
