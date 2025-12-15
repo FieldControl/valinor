4c5b96b475b489e2266a3a9753f8be0dbbead4bc
Vitor Zanchini Santini <115574189+vitorsantini@users.noreply.github.com>
2025-12-15 10:41:14 -0300

    chore: realizando configuração para o deploy do server

--- kanban/server/src/main.ts
+++ kanban/server/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: 'http://localhost:4200',
+    origin: '*',
   });
   
   const port = process.env.PORT || 3000;
